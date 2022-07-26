34d3ebdfb7d2f93d0dad46c3980e05828d66a1b5
lianfa23-meet <109899264+lianfa23-meet@users.noreply.github.com>
2022-07-26 10:31:49 +0300

    changes

--- Flask-Routing-Lab/app.py
+++ Flask-Routing-Lab/app.py
@@ -8,8 +8,13 @@ app = Flask(  # Create a flask app
 )
 
 # Your code should be below
+@app.route('/')
+def index():
+    return render_template('home.html')
 
-
+@app.route('/product')
+def product():
+    return render_template('product.html')
 
 
 # Your code should be above
